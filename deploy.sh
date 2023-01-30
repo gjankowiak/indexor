83802d99977fc6e261b27a098ee962f8c366a659
gapato <g@oknaj.eu>
2023-01-30 22:37:43 +0100

    allow local deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-scp index.html indexor.js main.js nuiton:/var/www/janko.fr/gaspard.static/enseignement/konstanz/utils/indexor/
+set -e
+set -x
+
+if [ $HOSTNAME == "nuiton" ];
+then
+  cp * ../indexor
+else
+  scp index.html indexor.js main.js nuiton:/var/www/janko.fr/gaspard.static/enseignement/konstanz/utils/indexor/
+fi
